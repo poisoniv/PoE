2bfb450d7ae583d2b9069a150f28bafaf713f8da
poisoniv <john.henise@gmail.com>
2014-06-26 20:42:57 -0700

    Create macros.h

---
+++ macros.h
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// Macro Configurations
+
+#define BIT0			0x01
+#define BIT1			0x02
+#define BIT2			0x04
+#define BIT3			0x08
+#define BIT4			0x10
+#define BIT5			0x20
+#define BIT6			0x40
+#define BIT7			0x80
+
+#define ulong   unsigned long
+#define uint	unsigned int
+#define uchar 	unsigned char
+
+#define TRUE	1
+#define FALSE	0
+
+#define PASSED  TRUE
+#define FAILED  FALSE
+
+#define YES		TRUE
+#define NO		FALSE
+
+#define ON		TRUE
+#define OFF		FALSE
+
+#define HIGH 1
+#define LOW 0
+

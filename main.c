a37b06249880e401d6243e862b5cce5dd87a71d6
poisoniv <john.henise@gmail.com>
2014-06-27 23:39:45 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,18 @@
 #include "io430.h"
+#include  "macros.h"
+#include  "functions.h"
 
 int main( void )
 {
-	// Stop watchdog timer to prevent time out reset
-	WDTCTL = WDTPW + WDTHOLD;
-	
-	// Disable the GPIO power-on default high-impedance mode
-	// to activate previously configured port settings
-	PM5CTL0 &= ~LOCKLPM5;
-
-	// Configure GPIO
-	P1DIR = 0x01; 
-	P1OUT = 0;                 
-					   
-	P4DIR = 0x40; 
-	P4OUT = 0;                
-
-	while(1)
-	{
-		P1OUT = 0x01;                // Toggle LED
-		P4OUT = 0x00; 
-		__delay_cycles(1000000);
-		P4OUT = 0x40;                // Toggle LED
-		P1OUT = 0x00;  
-		__delay_cycles(1000000);
-	}
-
+  initialize();
+  
+  LED1_ON();
+  LED2_OFF();
+  
+  while(TRUE)
+  {
+    LED1_TOGGLE();
+    LED2_TOGGLE();
+    __delay_cycles(1000000);    
+  }
 }

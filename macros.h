39b1ee4f1b77c475c37cd55570ff80a6b4d230b8
poisoniv <john.henise@gmail.com>
2014-06-27 23:39:09 -0700

    Update macros.h

--- macros.h
+++ macros.h
@@ -1,31 +1,38 @@
 //------------------------------------------------------------------------------
 // Macro Configurations
+//------------------------------------------------------------------------------
 
-#define BIT0			0x01
-#define BIT1			0x02
-#define BIT2			0x04
-#define BIT3			0x08
-#define BIT4			0x10
-#define BIT5			0x20
-#define BIT6			0x40
-#define BIT7			0x80
-
-#define ulong   unsigned long
-#define uint	unsigned int
-#define uchar 	unsigned char
+//#define BIT0	0x01
+//#define BIT1	0x02
+//#define BIT2	0x04
+//#define BIT3	0x08
+//#define BIT4	0x10
+//#define BIT5	0x20
+//#define BIT6	0x40
+//#define BIT7	0x80
 
 #define TRUE	1
 #define FALSE	0
 
-#define PASSED  TRUE
-#define FAILED  FALSE
+#define PASSED  1
+#define FAILED  0
 
-#define YES		TRUE
-#define NO		FALSE
+#define YES		1
+#define NO		0
 
-#define ON		TRUE
-#define OFF		FALSE
+#define ON		1
+#define OFF		0
 
-#define HIGH 1
-#define LOW 0
+#define HIGH 	1
+#define LOW 	0
+
+#define LED1 	p4_6
+#define LED2 	p1_0
+
+#define PORT_OUT        1
+#define PORT_IN         0
+
+#define PORT_HIGH       1
+#define PORT_LOW        0
+
 
